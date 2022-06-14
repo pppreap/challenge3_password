3aa7978d0e92fc835397667361f10969cce6cfaa
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-06-14 15:35:38 -0400

    added more comment or fixed incorrect comments

--- Develop/script.js
+++ Develop/script.js
@@ -16,25 +16,26 @@ function generatePassword() {
 //use prompt for  password criteria determination
 while (true) {
 var inputLength = Number(prompt("What is the desired password length?", "Enter a number between 8 and 128."));
-if (!inputLength) {
-  alert("Please enter valid input. Enter a number  from 8-128 characters");
-}
-else if  (inputLength < 8 || inputLength > 128) {
-prompt("Please enter valid value", " Enter a number from 8-128");
-}
+  if (!inputLength) {
+  alert("Please enter valid input. Enter a number from 8-128 characters");
+  }
+  else if  (inputLength < 8 || inputLength > 128) {
+  prompt("Please enter valid value", " Enter a number from 8-128");
+  }
 // if unacceptable input value ,  cannot move forward until correct input is entered in prompt
-else {
-break;
-}
+  else {
+  break;
+  }
 }
 
 // if acceptable input length, prompt next criteria of character types
 
 //declare confirmed variable options for password character types
-var confirmLowerCase = confirm(" Lowercase is selected");
-var confirmUpperCase = confirm(" Uppercase is selected");
-var confirmNum = confirm("Numeric is selected");
-var confirmSpecial = confirm("Special Character is selected");
+alert("Please confirm character types to be included in password. Click OK to confirm selection of various character types. Please select at least one type: lowercase, uppercase, numeric, and special characters");
+var confirmLowerCase = confirm(" Lowercase characters is selected");
+var confirmUpperCase = confirm(" Uppercase characters  is selected");
+var confirmNum = confirm("Numeric characters is selected");
+var confirmSpecial = confirm("Special Characters is selected");
 
 //if not at least one character type is selected, alert one character type  must be selected 
 if (confirmLowerCase === false && confirmUpperCase === false && confirmNum === false &&  confirmSpecial === false) {
@@ -45,7 +46,7 @@ if (confirmLowerCase === false && confirmUpperCase === false && confirmNum === f
   var confirmSpecial = confirm("Special Character is selected");
 }
  
-  //all types of options of character type selections with the joined arrays values via concat
+  //declared all options of character type selection, joined arrays values via concat method
 if (confirmLowerCase === true) {
   options = options.concat(lowercase);
 }
@@ -62,14 +63,14 @@ if (confirmSpecial === true) {
   options = options.concat(special);
 }
 
-var userOptionsPw =[]; // password created will be a string
+var userOptionsPw =[]; // password created will be input in to this empty variable
 // how to get a randomize password based on options and inputlength using Loop, push  and Math.random method
 for (var i= 0; i < inputLength; i++) {
  var optionsCombined = options[Math.floor(Math.random()* options.length)];
  userOptionsPw.push(optionsCombined);
 }
-
-return userOptionsPw.join("");
+//join the selected options value with input length to create the random password to return value 
+return userOptionsPw.join(""); 
 }
 
 // Get references to the #generate element
